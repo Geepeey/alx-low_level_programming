177b8500342ac9199cdcc0092a1071235cfdb036
Godspower <harshmallins@gmail.com>
2022-09-09 15:21:18 -0700

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,14 @@
  * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
+ *
  */
-
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-				putchar(letter);
+		putchar(letter);
 
 	putchar('\n');
 
